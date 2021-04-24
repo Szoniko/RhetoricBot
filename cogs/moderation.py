736350c93bb4febe95207d1782eed168f148f4be
Szoniko <miloszmigon99@gmail.com>
2021-04-24 17:39:06 +0200

    added moderation

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -2,7 +2,6 @@ import discord, config
 from discord.ext.commands.errors import MissingPermissions
 from discord.ext import commands
 
-
 class Moderation(commands.Cog):
 
     #initialize 
@@ -24,22 +23,22 @@ class Moderation(commands.Cog):
     async def kick(self, ctx, member: discord.Member, reason):
         try:
             if reason is not None:
-                member.kick(reason=reason)
+                await member.kick(reason=reason)
             else:
-                member.kick()
-            ctx.send(f"{member} was kicked by **{ctx.message.author}**.\n**REASON**: {reason}")
+                await member.kick()
+            await ctx.send(f"{member} was kicked by **{ctx.message.author}**.\n**REASON**: {reason}")
         except MissingPermissions:
-            ctx.send("You can't do that silly! *missing permissions*")
+            await ctx.send("You can't do that silly! *missing permissions*")
 
 
     @commands.command()
     @commands.has_permissions(ban_members=True)
     async def ban(self, ctx, member: discord.Member):
         try:
-            member.ban()
-            ctx.send(f"{member} was banned by **{ctx.message.author}**.")
+            await member.ban()
+            await ctx.send(f"{member} was banned by **{ctx.message.author}**.")
         except MissingPermissions:
-            ctx.send("You can't do that silly! *missing permissions*")
+            await ctx.send("You can't do that silly! *missing permissions*")
 
 
     @commands.command()
@@ -47,13 +46,37 @@ class Moderation(commands.Cog):
     async def unban(self, ctx, member: discord.Member, reason):
         try:
             if reason is not None:
-                member.unban(reason=reason)
+                await member.unban(reason=reason)
             else:
-                member.unban()
-            ctx.send(f"{member} was unbaned by **{ctx.message.author}**.\n**REASON**: {reason}")
+                await member.unban()
+            await ctx.send(f"{member} was unbaned by **{ctx.message.author}**.\n**REASON**: {reason}")
         except MissingPermissions:
-            ctx.send("You can't do that silly! *missing permissions*")
+            await ctx.send("You can't do that silly! *missing permissions*")
 
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def mute(self, ctx, member: discord.Member):
+        try:
+            role = discord.utils.get(member.guild.roles, name="muted")
+            await member.add_roles(role)
+            await ctx.send(f"{member} was muted by **{ctx.message.author}**")
+        except MissingPermissions:
+            await ctx.send("You can't do that silly! *missing permissions*")
+
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def unmute(self, ctx, member: discord.Member):
+        try:
+            role = discord.utils.get(member.guild.roles, name="muted")
+            await member.remove_roles(role)
+            await ctx.send(f"{member} was unmuted by {ctx.message.author}")
+        except MissingPermissions:
+            await ctx.send("You can't do that silly! *missing permissions*")
+    
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def lock(ctx, channel: discord.channel):
+        pass
 
 # don't touch this
 def setup(client):
