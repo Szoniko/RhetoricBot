e42029b63c5763a4701cf6550895a9bdc47663a5
Szoniko <miloszmigon99@gmail.com>
2021-06-25 13:31:19 +0200

    changed most of the commands to slash commands

--- config.py
+++ config.py
@@ -8,11 +8,19 @@ Please follow the instructions next to variables
 
 # discord bot token
 
-AUTH = "YOUR DISCORD BOT TOKEN HERE" # I hope you know how to get that
+AUTH = "ODQxMjgyODEzODMxNTQ0ODMy.YJkfrQ.9q8OYSOXW_QTAvslgp9UIMqsNtM" # I hope you know how to get that
 
 # hypixel api key
 
-HYPIXEL_API_KEY = "YOU API KEY HERE" # you can get it by running /api new in hypixel
+HYPIXEL_API_KEY = "c3e23ddf-e086-4ce5-b008-d3167270e7c5" # you can get it by running /api new in hypixel
+
+# Guild name
+
+GUILD_NAME = "Rhetoric"
+
+# gxp reqs 
+
+GXP_REQUIREMENT = 50000
 
 # The prefix of the bot
 
