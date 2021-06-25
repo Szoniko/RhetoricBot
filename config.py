554cf7c550cd4925f33812bd5387b98445002337
Szoniko <miloszmigon99@gmail.com>
2021-06-25 13:55:04 +0200

    remove keys

--- config.py
+++ config.py
@@ -8,11 +8,11 @@ Please follow the instructions next to variables
 
 # discord bot token
 
-AUTH = "ODQxMjgyODEzODMxNTQ0ODMy.YJkfrQ.9q8OYSOXW_QTAvslgp9UIMqsNtM" # I hope you know how to get that
+AUTH = "YOUR DISCORD BOT TOKEN HERE" # I hope you know how to get that
 
 # hypixel api key
 
-HYPIXEL_API_KEY = "c3e23ddf-e086-4ce5-b008-d3167270e7c5" # you can get it by running /api new in hypixel
+HYPIXEL_API_KEY = "YOUR API KEY HERE" # you can get it by running /api new in hypixel
 
 # Guild name
 
